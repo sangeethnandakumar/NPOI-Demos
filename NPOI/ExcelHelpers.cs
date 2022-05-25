8c68c02fa635d177089aa38fede01968cbb17290
Sangeeth Nandakumar <twileloop@outlook.com>
2022-05-26 03:44:25 +0530

    Add project files.

---
+++ NPOI/ExcelHelpers.cs
@@ -0,0 +1,49 @@
+﻿using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+
+namespace NPOI.Demo
+{
+    public static class ExcelHelpers
+    {
+        public static void AddRow(this ISheet sheet, XSSFCellStyle style, int rowNum,  params string[] values)
+        {
+            var row = sheet.CreateRow(rowNum);
+            for (var i=0; i<values.Length; i++)
+            {
+                var cell = row.CreateCell(i);
+                cell.CellStyle = style;                
+                cell.SetCellValue(values[i]);                   
+            }
+        }
+        public static void AddRow(this ISheet sheet, int rowNum, params string[] values)
+        {
+            var row = sheet.CreateRow(rowNum);
+            for (var i = 0; i < values.Length; i++)
+            {
+                row.CreateCell(i).SetCellValue(values[i]);
+            }
+        }
+
+        public static int LoadImage(this XSSFWorkbook workbook, byte[] image, PictureType format)
+        {
+            return workbook.AddPicture(image, format);
+        }            
+            public static void InsertImage(this ISheet sheet, XSSFWorkbook workbook, int imageIndex, int row, int column)
+        {
+           
+            ICreationHelper helper = workbook.GetCreationHelper();
+            IDrawing drawing = sheet.CreateDrawingPatriarch();
+            IClientAnchor anchor = helper.CreateClientAnchor();
+            anchor.Col1 = column;
+            anchor.Row1 = row;
+            IPicture picture = drawing.CreatePicture(anchor, imageIndex);
+            picture.Resize();
+        }        
+
+
+        public static void ColumnWidth(this ISheet sheet, int columnNum, int size)
+        {
+            sheet.SetColumnWidth(columnNum, 256*size);
+        }
+    }
+}
---
+++ NPOI/Program.cs
@@ -0,0 +1,72 @@
+﻿
+using NPOI.Demo;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+
+var workbook = new XSSFWorkbook();
+var sheet = workbook.CreateSheet("Sheet A");
+
+
+//Build global styles
+var style = new CellStyleBuilder(workbook)
+                        .SetFont("Areal")
+                        .SetFontColor(IndexedColors.Black)
+                        .SetBold(true)
+                        .SetBackground(FillPattern.Diamonds, IndexedColors.Green)
+                        .Style;
+
+var hyperlink = new CellStyleBuilder(workbook)
+                        .SetFont("Areal")
+                        .SetFontColor(IndexedColors.Blue)
+                        .SetBold(true)
+                        .SetBackground(FillPattern.SolidForeground, IndexedColors.White)
+                        .Style;
+
+//Defaul default styled and custom styled row values
+sheet.AddRow(0, "Sangeeth", "Nandakumar", "RREXS");
+sheet.AddRow(style, 1, "Navaneeth", "Nandakumar", "PPXE");
+sheet.AddRow(2, "Surya", "Nandakumar");
+sheet.AddRow(style, 3, "K", "Nandakumar");
+
+//Set col size
+sheet.ColumnWidth(0, 10);
+sheet.ColumnWidth(1, 20);
+sheet.ColumnWidth(2, 30);
+
+//Specific style for cell
+var cell = sheet.GetRow(0).GetCell(1);
+cell.CellStyle = hyperlink;
+
+//Making a cell hyperlink
+cell.Hyperlink = new XSSFHyperlink(HyperlinkType.Url)
+{
+    Address = "https://www.google.com"
+};
+
+//Load image to workbook
+byte[] data = File.ReadAllBytes(@"C:\Users\Sangeeth Nandakumar\OneDrive\Desktop\copilot.png");
+var imageIndex = workbook.LoadImage(data, PictureType.PNG);
+
+//Insert image from workbook by image index
+sheet.InsertImage(workbook, imageIndex , 0, 1);
+
+
+//Preset usefull styles
+//SUCCESS
+sheet.GetRow(0).GetCell(0).CellStyle = new CellStyleBuilder(workbook).DefaultSuccess().Style;
+sheet.GetRow(0).GetCell(0).SetCellValue("Success");
+//SUCCESS
+sheet.GetRow(0).GetCell(1).CellStyle = new CellStyleBuilder(workbook).DefaultWarning().Style;
+sheet.GetRow(0).GetCell(1).SetCellValue("Warning");
+//SUCCESS
+sheet.GetRow(0).GetCell(2).CellStyle = new CellStyleBuilder(workbook).DefaultError().Style;
+sheet.GetRow(0).GetCell(2).SetCellValue("Error");
+
+
+
+
+
+var xfile = new FileStream("test.xlsx", FileMode.Create, FileAccess.Write);
+workbook.Write(xfile);
+workbook.Close();
+xfile.Close();
